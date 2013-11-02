44ab7e5c32592d38ba96bde1954c4645ff27b0b1
viscent <viscent@viscent.(none)>
2013-11-02 22:58:32 +0800

    Change CSS Load test case

--- decjtest/js/module/cssDynaLoad/hybridCSS.js
+++ decjtest/js/module/cssDynaLoad/hybridCSS.js
@@ -1,7 +1,7 @@
 define(['jquery','decj'],function(jQuery,decj){
 
   return {
-    css:["visibility.css","http://static.your.testhost.com/decjtest/css/visibility.css","//static.your.testhost.com/decjtest/css/visibility1.css"],
+    css:["size.css","http://static.your.testhost.com/decjtest/css/visibility.css","//static.your.testhost.com/decjtest/css/visibility1.css"],
     init:function(){
      $('#loadingState').html('done');
     }
